74e9318ac606f9b3df4ee0b1c8721f673aca6e0e
bipinjlimbu <bipinlimbu712@gmail.com>
2026-07-24 11:05:57 +0545

    Profile Page Added

--- nf_project/nf_app/urls.py
+++ nf_project/nf_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
 from .views.main_view import home_page
+from .views.profile_view import profile_view
 
 urlpatterns = [
     path('', home_page, name='home'),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('profile/', profile_view, name='profile'),
 ]
