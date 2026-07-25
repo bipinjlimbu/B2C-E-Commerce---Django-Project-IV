8a15e890bfc1d755e7453131e8d673a39a09bed0
bipinjlimbu <bipinlimbu712@gmail.com>
2026-07-25 13:48:52 +0545

    Model Changes

--- nf_project/nf_app/models.py
+++ nf_project/nf_app/models.py
@@ -112,7 +112,6 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    selected_size = models.CharField(max_length=10, choices=Product.Size.choices, default=Product.Size.MEDIUM)
     price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
     
